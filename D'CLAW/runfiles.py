2edf8d49c3d2b72645a89dfeafdf843aa48681f7
ma1ze <843700172@qq.com>
2024-07-31 10:13:29 +0800

    branch init

--- D'CLAW/runfiles.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-subprocess.run(["python", "DRWKV6_ER.py"])
-subprocess.run(["python", "DRWKV5_ER.py"])
-subprocess.run(["python", "DRWKV4_ER.py"])
-subprocess.run(["python", "DT_ER_update.py"])
